2eb3bfa1a5b88da3da83960e4024030bc77c9b9f
Mikio Hirabayashi <hirarin@gmail.com>
2025-03-05 09:03:32 +0900

    The initial commit.

---
+++ sample.js
@@ -0,0 +1,19 @@
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+
+    if (num % 2 === 0 || num % 3 === 0) return false;
+
+    const limit = Math.sqrt(num);
+    for (let i = 5; i <= limit; i += 6) {
+        if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+
+    return true;
+}
+
+for (let i = 1; i <= 1000; i++) {
+    if (isPrime(i)) {
+        console.log(i);
+    }
+}
